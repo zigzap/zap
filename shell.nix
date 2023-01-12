e50072ebe3389b6fa0874d638250e4b7d928f1e0
Rene Schallner <rene@renerocks.ai>
2023-01-12 18:08:01 +0100

    added shell.nix for my convenience

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{
+    pkgs ? import <nixpkgs> { 
+        overlays = [ 
+            (import (builtins.fetchTarball {
+              # url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+              url = https://github.com/nix-community/neovim-nightly-overlay/archive/72ff8b1ca0331a8735c1eeaefb95c12dfe21d30a.tar.gz;
+            }))
+        ]; 
+    }
+} :
+pkgs.mkShell {
+    nativeBuildInputs = [ pkgs.neovim-nightly pkgs.bat ];
+}
