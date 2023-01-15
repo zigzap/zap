12028e53b2b54a84e8cfe075e99a540a07de1c43
Rene Schallner <rene@renerocks.ai>
2023-01-15 06:45:01 +0100

    forgot to check in rust code

---
+++ wrk/rust/hello/src/main.rs
@@ -0,0 +1,43 @@
+use hello::ThreadPool;
+// use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+// use std::thread;
+// use std::time::Duration;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    // for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+
+    let status_line = "HTTP/1.1 200 OK";
+
+    let contents = "HELLO from RUST!";
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
