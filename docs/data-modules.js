0d712282e241612cc32f187be1265d6b947acb28
Rene Schallner <rene@renerocks.ai>
2024-02-23 20:35:54 +0100

    doc update

--- docs/data-modules.js
+++ docs/data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"zap","file":0,"main":66,"table":{"zap":0}},{"name":"builtin","file":0,"main":454,"table":{}}];
+var modules =[{"name":"zap","file":0,"main":66,"table":{"zap":0}},{"name":"builtin","file":0,"main":463,"table":{}}];
